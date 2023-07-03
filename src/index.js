d37901f95bc74146eb00774d09eee39f41538d26
kevinjn03 <114575604+KevinJN03@users.noreply.github.com>
2023-07-03 02:43:32 +0100

    fix bug with humidity and breeze image

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import domWeather from "./modules/DomWeather"
 import "./style.css"
 let input = document.querySelector("[data-input]")
 let submitBtn = document.querySelector("[data-form-btn]")
-
+import "./Images/wind.png";
+import "./Images/humidity.png";
 startUp()
 submitBtn.addEventListener("click", async (e) => {
     e.preventDefault()
